8d81a05b29e8fd2360baecb129b28e36817d52b9
ginomcfino <jiweiqi717@gmail.com>
2024-02-06 23:35:02 -0600

    still testing INMP441

--- Circuit/test_INMP441_2/test_INMP441_2.ino
+++ Circuit/test_INMP441_2/test_INMP441_2.ino
@@ -1,9 +1,69 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <driver/i2s.h>
 
+#define I2S_WS 25
+#define I2S_SD 32
+#define I2S_SCK 33
+
+// use I2S processor 0
+#define I2S_PORT I2S_NUM_0
+
+#define bufferLen 64
+int16_t sBuffer[bufferLen];
+
+void i2s_install() {
+  const i2s_config_t i2s_config{
+    .mode = i2s_mode_t(I2S_MODE_MASTER | I2S_MODE_RX),
+    .sample_rate = 44100,
+    .bits_per_sample = i2s_bits_per_sample_t(16),
+    .channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,
+    .communication_format = i2s_comm_format_t(I2S_COMM_FORMAT_STAND_I2S),
+    .intr_alloc_flags = 0,
+    .dma_buf_count = 8,
+    .dma_buf_len = bufferLen,
+    .use_apll = false
+  };
+
+  i2s_driver_install(I2S_PORT, &i2s_config, 0, NULL);
+}
+
+void i2s_setpin() {
+  const i2s_pin_config_t pin_config = {
+    .bck_io_num = I2S_SCK,
+    .ws_io_num = I2S_WS,
+    .data_out_num = -1,
+    .data_in_num = I2S_SD
+  };
+
+  i2s_set_pin(I2S_PORT, & pin_config);
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  Serial.println(">>> STARTING ... ");
+
+  delay(1000),
+
+  i2s_install();
+  i2s_setpin();
+
+  i2s_start(I2S_PORT);
+
+  delay(500);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  size_t bytesIn = 0;
 
+  esp_err_t result = i2s_read(I2S_PORT, &sBuffer, bufferLen, &bytesIn, portMAX_DELAY);
+
+  if (result == ESP_OK){
+    int16_t samples_read = bytesIn / 2; // each sample is 2 bytes
+    
+    if (samples_read > 0) {
+      for (int16_t i = 0; i < samples_read; ++i) {
+        Serial.println(sBuffer[i]);
+      }
+    }
+  }
 }
